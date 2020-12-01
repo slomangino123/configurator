dfd38651829cf1ca83e7647efcabfb6017f67658
Stephen Lomangino <iceman371@yahoo.com>
2020-11-30 23:43:33 -0500

    updated run script to also add environment variables through docker.

--- samples/Configurator.Sample.AspNetCore/run.ps1
+++ samples/Configurator.Sample.AspNetCore/run.ps1
@@ -1,1 +1,8 @@
-docker run -v "$(pwd)\samples\Configurator.Sample.AspNetCore\:/output" configurator:aspnetcore
+docker run `
+-v "$(pwd)\samples\Configurator.Sample.AspNetCore\output:/output" `
+-e build=true `
+-e branch=from-docker-file `
+-e test=derp `
+-e YAML_OUTPUT_PATH="/output" `
+-e YAML_FILENAME="test" `
+configurator:aspnetcore
