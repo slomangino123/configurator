5bbe5957dc942858688b5ea7625fb7a14846956d
Stephen Lomangino <iceman371@yahoo.com>
2020-12-01 23:53:52 -0500

    Fixed references

--- samples/Configurator.Sample.AspNetCore/Services/Validators/BuildValidator.cs
+++ samples/Configurator.Sample.AspNetCore/Services/Validators/BuildValidator.cs
@@ -2,7 +2,7 @@
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Configurator.Configuration;
+using Configurator.Sample.AspNetCore.Objects;
 using Configurator.Validator;
 
 namespace Configurator.Sample.AspNetCore.Services.Validators
